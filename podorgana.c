d7dc688d21cbb9429ae6e62a9321e958a648edc7
Jon Daniel <jopadan@ctemplar.com>
2021-03-04 12:56:53 +0000

    Switch to POD5 for testing

--- podorgana.c
+++ podorgana.c
@@ -14,36 +14,38 @@ int main(int argc, char** argv)
 		pod_path_t src = strdup(argv[1]);
 		if(src == NULL)
 			fprintf(stderr, "ERROR: path == NULL!");
+
 		printf("native path: %s\n", src);
 
-		pod_file_pod2_t* pod2 = pod_file_pod2_create(src);
-		if(pod2 == NULL)
+		/* open POD5 file copied from argv[1] */
+		pod_file_pod5_t* pod5 = pod_file_pod5_create(src);
+		if(pod5 == NULL)
 		{
-			fprintf(stderr, "ERROR: cannot create pod2 file!\n");
+			fprintf(stderr, "ERROR: cannot create pod5 file!\n");
 			exit(EXIT_FAILURE);
 		}
 
-		pod_file_pod2_print(pod2);
+		pod_file_pod5_print(pod5);
 		
-
-		if(!pod_file_pod2_write(pod2, "out.pod"))
+/*
+		if(!pod_file_pod5_write(pod5, "out.pod"))
 		{
 			fprintf(stderr, "ERROR: cannot write out.pod!\n");
-			pod_file_pod2_destroy(pod2);
+			pod_file_pod5_destroy(pod5);
 			exit(EXIT_FAILURE);
 		}
 
 		
 		pod_path_t dst = pod_path_append_posix(argv[2], pod_path_trim(src));
 		printf("dst: %s trimmed: %s\n", src, dst);
-		if(!pod_file_pod2_extract(pod2, dst, false))
+		if(!pod_file_pod5_extract(pod5, dst, false))
 		{
-			fprintf(stderr, "ERROR: pod_file_pod2_extract(%s) failed!\n", dst);
-			pod_file_pod2_destroy(pod2);
+			fprintf(stderr, "ERROR: pod_file_pod5_extract(%s) failed!\n", dst);
+			pod_file_pod5_destroy(pod5);
 			exit(EXIT_FAILURE);
 		}
-
-		pod_file_pod2_destroy(pod2);
+*/
+		pod_file_pod5_destroy(pod5);
 		free(src);
 	}
 	else
